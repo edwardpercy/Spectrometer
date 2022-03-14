584a34236b7fbb4fab50bfc7776c367fbd24c3da
Edward Percy <percy.edward74@gmail.com>
2022-03-14 16:05:59 +0000

    screen test

--- screenTest.py
+++ screenTest.py
@@ -1,8 +1,12 @@
 from papirus import PapirusComposite
-
+import time
 
 textNImg = PapirusComposite()
 
 # Write text to the screen at selected point, with an Id
 # Nothing will show on the screen
-textNImg.AddText("hello world", 10, 10, Id="Start" )
+for n in range (100):
+    textNImg.AddText(f"Test {n}", 10, 10, Id="Start" )
+    textNImg.UpdateText("Start", f"Test {n}")
+    textNImg.WriteAll()
+    time.sleep(1)
