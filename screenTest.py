95efeb1e560324ef1382f9e51423d60eb7120be2
Edward Percy <percy.edward74@gmail.com>
2022-03-14 16:07:44 +0000

    screen test

--- screenTest.py
+++ screenTest.py
@@ -5,8 +5,10 @@ textNImg = PapirusComposite()
 
 # Write text to the screen at selected point, with an Id
 # Nothing will show on the screen
+x = 0
+textNImg.AddText(str(x), 10, 10, Id="Start" )
 for n in range (100):
-    textNImg.AddText(str(n), 10, 10, Id="Start" )
-    textNImg.UpdateText("Start", str(n))
+    textNImg.UpdateText("Start", str(x))
     textNImg.WriteAll()
-    time.sleep(1)
+    time.sleep(0.5)
+    x += 1
