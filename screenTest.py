5161359c77192cd652d4ffa125546b2fbf186863
Edward Percy <percy.edward74@gmail.com>
2022-03-14 16:00:25 +0000

    screen test

--- screenTest.py
+++ screenTest.py
@@ -3,6 +3,4 @@ from papirus import PapirusText
 
 text = PapirusText(90)
 
-# Write text to the screen, in this case forty stars alternating black and white
-# note the use of u"" syntax to specify unicode (needed for Python 2, optional for Python 3 since unicode is default in Python 3)
-text.write(u"\u2605 \u2606 \u2605 \u2606 \u2605 \u2606 \u2605 \u2606 \u2605 \u2606 \u2605 \u2606 \u2605 \u2606 \u2605 \u2606 \u2605 \u2606 \u2605 \u2606 \u2605 \u2606 \u2605 \u2606 \u2605 \u2606 \u2605 \u2606 \u2605 \u2606 \u2605 \u2606 \u2605 \u2606 \u2605 \u2606 \u2605 \u2606 \u2605 \u2606")
+text.AddText("hello world", 10, 10, Id="Start" )
