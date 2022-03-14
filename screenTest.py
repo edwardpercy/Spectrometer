9ba3b96d4bd81b5d9ec53e63a6e2e20f98173cd9
Edward Percy <percy.edward74@gmail.com>
2022-03-14 15:59:27 +0000

    screen test

--- screenTest.py
+++ screenTest.py
@@ -1,7 +1,7 @@
 from papirus import PapirusText
 
 
-text = PapirusText([rotation = 90])
+text = PapirusText(90)
 
 # Write text to the screen, in this case forty stars alternating black and white
 # note the use of u"" syntax to specify unicode (needed for Python 2, optional for Python 3 since unicode is default in Python 3)
