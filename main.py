b5d18086af6015d404bb266ccb7c83a0573cd623
Edward Percy <percy.edward74@gmail.com>
2022-03-30 13:32:01 +0100

    Combining Functions

--- main.py
+++ main.py
@@ -73,9 +73,8 @@ stepperHandler = StepperHandler(STEP_PIN, DIRECTION_PIN, 0.01)
 stepperHandler.Step(100, stepperHandler.ANTI_CLOCKWISE)
 stepperHandler.home()
 
+result = subprocess.run([sys.executable, "adc_control.py"])
 
-opener = "open" if sys.platform == "darwin" else "xdg-open"
-subprocess.call([opener, 'adc_control.py'])
 sleep(2)
 # Go backwards once
 GPIO.output(RELAY_PIN, GPIO.HIGH)
